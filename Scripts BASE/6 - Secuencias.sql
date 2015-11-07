6d65801c4170a48dc1dbb9905adca4bcbbbd5837
andreysanchezcr <andreysanchezcr@gmail.com>
2015-11-06 19:44:53 -0600

    secuencias

---
+++ Scripts BASE/6 - Secuencias.sql
@@ -0,0 +1,136 @@
+create sequence seq_actType
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_act_x_player
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_admin
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_align
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_award
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_city
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_country
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_event
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_event_x_team
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_groupp
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_match
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_parameter
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_player
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_player_x_position
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_position
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_stadium
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
+create sequence seq_team
+minvalue 0
+maxvalue 100000
+start with 0
+increment by 1
+cache 20
+order;
+
