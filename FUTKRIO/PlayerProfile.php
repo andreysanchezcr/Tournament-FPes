f4e78c18cfa7486f3d8e47b79df3ff2055cf3f8f
KRVJ <kramirezvegaj@hotmail.com>
2015-11-10 12:00:03 -0600

    Finalizada 99% PlayerProfile

--- FUTKRIO/PlayerProfile.php
+++ FUTKRIO/PlayerProfile.php
@@ -15,14 +15,14 @@ $ret = ociexecute($outrefc); // Execute cursor
 $nrows = ocifetchstatement($outrefc, $data); // fetch data from cursor
 ocifreestatement($mycursor); // close procedure call
 ocifreestatement($outrefc); // close cursor
-
-for($i;$i<count($data['ID_PLAYER']);$i++){
-	$nombre=$data['first_name'][$i];
-	$apellido=$data['last_name'][$i];
-	$nickname=$data['nickname'][$i];
-	$numeroCamisa=$data['t_shirt_num'][$i];
-	$foto=$data['photo'][$i];
-	$pais=$data['name_country'][$i];
+$largo=count($data['ID_PLAYER']);
+for($i=0;$i<count($data['ID_PLAYER']);$i++){
+	$nombre=$data['FIRST_NAME'][$i];
+	$apellido=$data['LAST_NAME'][$i];
+	$nickname=$data['NICKNAME'][$i];
+	$numeroCamisa=$data['T_SHIRT_NUM'][$i];
+	$foto=$data['PHOTO'][$i];
+	$pais=$data['NAME_COUNTRY'][$i];
 }
 $premios="";
 
@@ -35,7 +35,7 @@ $nrows = ocifetchstatement($outrefc, $data); // fetch data from cursor
 ocifreestatement($mycursor); // close procedure call
 ocifreestatement($outrefc); // close cursor
 
-for($i;$i<count($data['NAME_AWARD']);$i++){
+for($i=0;$i<count($data['NAME_AWARD']);$i++){
 	if($i<count($data['NAME_AWARD']-1)){
 		$premios=$premios.$data['NAME_AWARD'][$i]-"$$";
 	}else{
@@ -51,14 +51,37 @@ OCILogoff($conn);
 		<title>Jugadores</title> 
 		<meta charset = "utf-8"/>
 		<meta description ="Love Ink una pagina web que no robara tu informacion para conquistar el mundo">
-
 		<link  rel="stylesheet" type="text/css" href="css/PlayerProfile.css"/>
 		<link  rel="stylesheet" type="text/css" href="css/input-Style.css"/>
 		<link rel="stylesheet" type="text/css" href="css/select-Style.css">
-
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script language="javascript" type="text/javascript" src="js/InputAnimation.js"></script>
-
+		<script type="text/javascript"> 
+			function Mostrar_Perfil_Jugador(nombre,apodo,nacionalidad,premios,numCamisa,nomCamisa){
+					var Caja_Jugadores = document.getElementById("caja_Jugadores");
+				     var card_html = ''+
+				            '<div class = "Jugador_Carta">'+
+				      '<div class ="caja_Foto">'+
+				            '<img class ="resizesable" src= "	http://www.nosoloposters.com/3749-large_default/postal-real-madrid-keylor-navas-2014-15.jpg"/>'+
+				        '<div class="t-shirt-name">'+nomCamisa+'</div>'+
+				        '<div class="t-shirt">'+numCamisa+'</div>'+
+				      '</div>'+
+				      '<div class = "Jugador_Info">'+
+				        '<div class = "Jugador_Description">'+
+				          '<h1>'+nombre+'</h1>'+
+				          '<h4>Apodo: '+apodo +'</h4>'+
+				          '<h4>Nacionalidad: '+nacionalidad+'</h4>'+
+				          
+				        '</div>'+
+				        '<div class="caja_Premios"><div class="premios_titulo">Premios Ganados</div>';
+				        premios=premios.split("$$");
+				        for(var i=0;i<premios.lenght;i++){  
+				          card_html+='<div class="award">'+premios[i]+'</div>';
+				        }
+				       card_html+=' </div>'+
+				      '</div>'+
+				    '</div> ';
+				Caja_Jugadores.innerHTML= card_html;
+			}
+		</script>
 	</head>
 	<body>
   <div id = "caja_Jugadores">
@@ -66,6 +89,8 @@ OCILogoff($conn);
   </div>
 </body>
 <?php
+
+
 	$nombre=$nombre." ".$apellido;
-	echo "<script type='text/javascript'>Mostrar_Perfil_Jugador('$nombre','$nickname','$pais','$premios','$numeroCamisa','$nickname');</script>"; 
+	echo "<script type='text/javascript'>Mostrar_Perfil_Jugador('$nombre','$nickname','$pais','$premios','$numeroCamisa','$nickname');</script>";
 ?>
