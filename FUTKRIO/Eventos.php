986a6626ae4608c924518cd3018eb4a427ad3b5d
andreysanchezcr <andreysanchezcr@gmail.com>
2015-11-11 12:02:16 -0600

    Visualizar informacion eventos

--- FUTKRIO/Eventos.php
+++ FUTKRIO/Eventos.php
@@ -1,11 +1,17 @@
 <?php
 include 'js/Eventos_PHP_JS.php';
 include 'html/menuPrincipal.php';
+include ("conexion.php");
+
 $array_premios = array("Balon Oro", "$1000", "Un Barriton","Una Chica");
+
 $mov_str = implode(",", $array_premios);
 //$id=$_GET["id"];
 
+
+
 ?>
+
 
 <head>
   <meta charset="utf-8" />
@@ -20,15 +26,34 @@ $mov_str = implode(",", $array_premios);
   <a href="#" onclick="ClearEventoHombres();">clear</a>
   <div class="Evento_Seg_Titulo">HOMBRES</div>
   <div id="Eventos_Hombres" class="ContenerdorEventos">
-    <div class="Evento">
-      
-      <div class="Evento_Info_Box">
-        <a href="#">
-          <div class="Evento_fecha">marz 20</div>
-          <div class="Evento_Nombre">ddddd</div>
+
+<?php
+$conn = OCILogon($user, $pass, $db);
+
+
+$outrefc = ocinewcursor($conn); //Declare cursor variable
+
+$mycursor = ociparse ($conn, "begin getInfoEvents(:curs,0); end;"); // prepare procedure call
+ocibindbyname($mycursor, ':curs', $outrefc, -1, OCI_B_CURSOR); // bind procedure parameters
+$ret = ociexecute($mycursor); // Execute function
+$ret = ociexecute($outrefc); // Execute cursor
+$nrows = ocifetchstatement($outrefc, $listaEventos); // fetch data from cursor
+ocifreestatement($mycursor); // close procedure call
+ocifreestatement($outrefc); // close cursor
+
+
+  for($p=0;$p<count($listaEventos["NAME_EVENT"]);$p++){
+    $nombreEvento=$listaEventos["NAME_EVENT"][$p];
+    $fechaInicio=$listaEventos["START_DATE"][$p];
+    $fechaFin=$listaEventos["END_DATE"][$p];
+    echo "<div class='Evento'>   
+      <div class='Evento_Info_Box'>
+        <a href='#''>
+          <div class='Evento_fecha'> $fechaInicio -- $fechaFin </div>
+          <div class='Evento_Nombre'> $nombreEvento </div>
         </a>
-        <div class="Evento_Premios">
-          <a href="#">
+        <div class='Evento_Premios'>
+          <a href='#'>
             <table>
               <caption>Premios</caption>
               <tr>
@@ -36,13 +61,21 @@ $mov_str = implode(",", $array_premios);
                 <td>,</td>
                 <td>,</td>
               </tr>
-
-
             </table>
           </a>
         </div>
       </div>
-    </div>
+    </div>";
+
+        
+    
+
+    }
+
+
+?>
+  
+
 
 
 
@@ -50,6 +83,53 @@ $mov_str = implode(",", $array_premios);
   </div>
   <div class="Evento_Seg_Titulo">MUJERES</div>
   <div id="Eventos_Mujeres" class="ContenerdorEventos">
+<?php
+$conn = OCILogon($user, $pass, $db);
+
+
+$outrefc = ocinewcursor($conn); //Declare cursor variable
+
+$mycursor = ociparse ($conn, "begin getInfoEvents(:curs,1); end;"); // prepare procedure call
+ocibindbyname($mycursor, ':curs', $outrefc, -1, OCI_B_CURSOR); // bind procedure parameters
+$ret = ociexecute($mycursor); // Execute function
+$ret = ociexecute($outrefc); // Execute cursor
+$nrows = ocifetchstatement($outrefc, $listaEventos); // fetch data from cursor
+ocifreestatement($mycursor); // close procedure call
+ocifreestatement($outrefc); // close cursor
+
+
+  for($p=0;$p<count($listaEventos["NAME_EVENT"]);$p++){
+    $nombreEvento=$listaEventos["NAME_EVENT"][$p];
+    $fechaInicio=$listaEventos["START_DATE"][$p];
+    $fechaFin=$listaEventos["END_DATE"][$p];
+    echo "<div class='Evento'>   
+      <div class='Evento_Info_Box'>
+        <a href='#''>
+          <div class='Evento_fecha'> $fechaInicio -- $fechaFin </div>
+          <div class='Evento_Nombre'> $nombreEvento </div>
+        </a>
+        <div class='Evento_Premios'>
+          <a href='#'>
+            <table>
+              <caption>Premios</caption>
+              <tr>
+                <td>,</td>
+                <td>,</td>
+                <td>,</td>
+              </tr>
+            </table>
+          </a>
+        </div>
+      </div>
+    </div>";
+
+        
+    
+
+    }
+
+
+?>
 
   </div>
 </body>
--- Scripts BASE/Nuevas Funciones Andrey.sql
+++ Scripts BASE/Nuevas Funciones Andrey.sql
@@ -1,3 +1,19 @@
+
+CREATE OR REPLACE PROCEDURE filtroEstadios(p_recordset out sys_refcursor, pais in varchar2, ciudadd in varchar2, nombre in varchar2) as
+begin
+  open p_recordset for
+
+  select stadium.id_stadium,stadium.name_stadium,stadium.capasity,stadium.photo,stadium.description,stadium.fk_city_id from stadium
+  where (getIDCountry(pais)=getFkCity(ciudadd) or pais is null or pais='' or ciudadd is null or ciudadd='')  and (stadium.name_stadium like nombre || '%' or nombre is null or nombre='');
+  
+  exception
+    when NO_DATA_FOUND THEN
+      NULL;
+      WHEN OTHERS THEN
+        RAISE;
+END filtroEstadios;
+
+
 CREATE OR REPLACE FUNCTION getIDCountry(pais in varchar2)
 RETURN NUMBER
 IS cantidad NUMBER(6);
@@ -16,16 +32,38 @@ BEGIN
 END;
 
 
-CREATE OR REPLACE PROCEDURE filtroEstadios(p_recordset out sys_refcursor, pais in varchar2, ciudadd in varchar2, nombre in varchar2) as
+
+
+
+
+
+CREATE OR REPLACE PROCEDURE getInfoEvents(p_recordset out sys_refcursor) as
 begin
   open p_recordset for
 
-  select stadium.id_stadium,stadium.name_stadium,stadium.capasity,stadium.description,stadium.fk_city_id from stadium
-  where (getIDCountry(pais)=getFkCity(ciudadd) or pais is null or pais='' or ciudadd is null or ciudadd='')  and (stadium.name_stadium like nombre || '%' or nombre is null or nombre='');
-  
+  select event.id_event,event.name_event,event.start_date,event.end_date from event;
+
+
   exception
     when NO_DATA_FOUND THEN
       NULL;
       WHEN OTHERS THEN
         RAISE;
-END filtroEstadios;
+END getInfoEvents;
+
+-- Se pasa por parametro el ID del evento
+CREATE OR REPLACE FUNCTION getPhotoAward(evento in number)
+RETURN blob
+IS foto blob;
+BEGIN
+   select award.photo into foto from award where award.fk_event_id=evento;
+   return foto;
+END;
+
+insert into event(id_event,name_event,start_date,end_date)
+values(0,'Nombre del evento',TO_DATE('01-03-2011','DD-MM-YYYY'),TO_DATE('01-03-2015','DD-MM-YYYY'));
+commit;
+
+select * from event;
+
+
