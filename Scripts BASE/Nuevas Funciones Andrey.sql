cca6a5aa3e6392db977128974e16dfcb094bcf6d
andreysanchezcr <andreysanchezcr@gmail.com>
2015-11-11 11:06:45 -0600

    Arreglo filtroEstadios

---
+++ Scripts BASE/Nuevas Funciones Andrey.sql
@@ -0,0 +1,33 @@
+
+CREATE OR REPLACE PROCEDURE filtroEstadios(p_recordset out sys_refcursor, pais in varchar2, ciudadd in varchar2, nombre in varchar2) as
+begin
+  open p_recordset for
+
+  select stadium.id_stadium,stadium.name_stadium,stadium.capasity,stadium.photo,stadium.description,stadium.fk_city_id from stadium
+  where (getIDCountry(pais)=getFkCity(ciudadd) or pais is null or pais='' or ciudadd is null or ciudadd='')  and (stadium.name_stadium like nombre || '%' or nombre is null or nombre='');
+  
+  exception
+    when NO_DATA_FOUND THEN
+      NULL;
+      WHEN OTHERS THEN
+        RAISE;
+END filtroEstadios;
+
+
+CREATE OR REPLACE FUNCTION getIDCountry(pais in varchar2)
+RETURN NUMBER
+IS cantidad NUMBER(6);
+BEGIN
+   select country.id_country into cantidad from country where country.name_country=pais;
+   return cantidad;
+END;
+
+
+CREATE OR REPLACE FUNCTION getFkCity(ciudad in varchar2)
+RETURN NUMBER
+IS cantidad NUMBER(6);
+BEGIN
+   select city.fk_country_id into cantidad from city where city.name_city=ciudad;
+   return cantidad;
+END;
+
