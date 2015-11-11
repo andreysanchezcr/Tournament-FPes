13f4cc0b8334d6b43213541cec6dab0e3b48c75b
andreysanchezcr <andreysanchezcr@gmail.com>
2015-11-11 14:37:21 -0600

    s

--- FUTKRIO/BuscarEquipos.php
+++ FUTKRIO/BuscarEquipos.php
@@ -1,6 +1,6 @@
 <?php 
-	include'js/BuscarEquiposJS.php';
-	    include 'html/menuPrincipal.php';
+  include'js/BuscarEquiposJS.php';
+      include 'html/menuPrincipal.php';
  ?>
 
   <head>
@@ -9,11 +9,11 @@
     <meta charset = "utf-8"/>
  
 
-	<link  rel="stylesheet" type="text/css" href="css/input-Style.css"/>
-	<link rel="stylesheet" type="text/css" href="css/select-Style.css">  
+  <link  rel="stylesheet" type="text/css" href="css/input-Style.css"/>
+  <link rel="stylesheet" type="text/css" href="css/select-Style.css">  
     <link  rel="stylesheet" type="text/css" href="css/BuscarEquiposCSS.css"/>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script language="javascript" type="text/javascript" src="js/InputAnimation.js"></script>
+    <script language="javascript" type="text/javascript" src="js/InputAnimation.js"></script>
 
   </head>
 
@@ -26,12 +26,12 @@
 <div class = "SearchTeamsBox">
 
   <div class = "SearchMenu">
-    		<div  class="mat-div">
-				    <div id="Label_Name_Search">
-	    				<label  for="first-name" class="mat-label">Buscar por Nombre</label>
-	    				<input  type="text" class="mat-input" id="nombre_jugador_Menu"></input>
-  					</div>
-			</div>
+        <div  class="mat-div">
+            <div id="Label_Name_Search">
+              <label  for="first-name" class="mat-label">Buscar por Nombre</label>
+              <input  type="text" class="mat-input" id="nombre_jugador_Menu"></input>
+            </div>
+      </div>
     <input type ="button" value ="Buscar"> </input>
 
 
