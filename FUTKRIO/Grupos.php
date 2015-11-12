0f1366911b2d6be94ed9801f769bdf37873cdbca
KRVJ <kramirezvegaj@hotmail.com>
2015-11-11 19:29:22 -0600

    COMMIT LUIS
    A LUIS SE LE JODIO EL GIT HUT

--- FUTKRIO/Grupos.php
+++ FUTKRIO/Grupos.php
@@ -1,5 +1,6 @@
 <?php  
 	include 'js/Grupos.php';
+	include 'html/menuPrincipal.php';
 ?>
 
   <head>
@@ -12,57 +13,12 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script language="javascript" type="text/javascript" src="js/calendar.js"></script>
 
-
-
-
-
-
-
 <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/themes/sunny/jquery-ui.css" />
 <script src="//code.jquery.com/jquery-latest.min.js"></script>
 <!-- script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script -->
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-
-
-
-
   </head>
-
   <body>
-  	
-<div id="Evento_U"class="Evento_U">
-  <div id="NombreEvento"class="Evento_Nombre">Eventicimo</div>
-  <div class="Fechas">
-    <div class="Fecha">18/7/210</div>
-    <div class="del_al"> hasta </div>
-    <div class="Fecha">19/3123/qw</div>
-  </div>
-  <div id ="descripcion"class="Descripcion">ascascascascascascascascascascascascascsacccccccccccccccc</div>
-  <div id="GroupBox" class="GroupBox">
-    <div class="UnGrupo">
-      <div class="GroupName">Grupo A</div>
-      <div class="TeamRow">
-        <div class="ElTeamName">Cos ddvsvsta Rica</div>
-        <div class="stak partidos_Jugados">1</div>
-        <div class="stak partidos_Ganados">2</div>
-        <div class="stak partidos_Perdidos">3</div>
-        <div class="stak partidos_Empatados">4</div>
-        <div class="stak partidos_GolFavor">5</div>
-        <div class="stak partidos_GolContra">6</div>
-        <div class="stak partidos_GolDiferencia">7</div>
-        <div class="stak partidos_FairPlay">8</div>
-      </div>
-    </div>
-    
-    
-    
-    
-    
-    
-  </div>
-</div>
-
-
 <a href="#" onclick="holas()">hoal</a>
 <div class="Caja_Principal_Editar">
   <div class="Caja_Info_Editar">
