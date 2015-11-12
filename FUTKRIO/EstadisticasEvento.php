7564894929c85755cd8a34d15b421bf54a4e0407
andreysanchezcr <andreysanchezcr@gmail.com>
2015-11-11 22:47:35 -0600

    estadisticas

--- FUTKRIO/EstadisticasEvento.php
+++ FUTKRIO/EstadisticasEvento.php
@@ -1,5 +1,11 @@
 <?php  
 		include 'html/menuPrincipal.php';
+
+
+      include ("conexion.php");
+      $conn = OCILogon($user, $pass, $db);
+
+
 ?>
 
   <head>
@@ -30,28 +36,143 @@
 
 <body>
 
-<div id="Evento_U"class="Evento_U">
-  <div id="NombreEvento"class="Evento_Nombre">Eventicimo</div>
-  <div class="Fechas">
-    <div class="Fecha">18/7/210</div>
-    <div class="del_al"> hasta </div>
-    <div class="Fecha">19/3123/qw</div>
+
+
+
+      <?php  
+
+    $equipo=0;
+    $evento=103;
+
+    $outrefc = ocinewcursor($conn); //Declare cursor variable
+    $mycursor = ociparse ($conn, "begin getInfoEventsById(:curs, $evento) ; end;"); // prepare procedure call
+    ocibindbyname($mycursor, ':curs', $outrefc, -1, OCI_B_CURSOR); // bind procedure parameters
+    $ret = ociexecute($mycursor); // Execute function
+    $ret = ociexecute($outrefc); // Execute cursor
+    $nrows = ocifetchstatement($outrefc, $infoEvento); // fetch data from cursor
+    ocifreestatement($mycursor); // close procedure call
+    ocifreestatement($outrefc); // close cursor
+
+    //var_dump($infoEvento);
+
+    $nombreEvento=$infoEvento['NAME_EVENT'][0];
+    $inicio=$infoEvento['START_DATE'][0];
+    $fin=$infoEvento['END_DATE'][0];
+
+
+    $outrefc = ocinewcursor($conn); //Declare cursor variable
+    $mycursor = ociparse ($conn, "begin getEquiposxGrupoxEvento(:curs, $evento) ; end;"); // prepare procedure call
+    ocibindbyname($mycursor, ':curs', $outrefc, -1, OCI_B_CURSOR); // bind procedure parameters
+    $ret = ociexecute($mycursor); // Execute function
+    $ret = ociexecute($outrefc); // Execute cursor
+    $nrows = ocifetchstatement($outrefc, $data2); // fetch data from cursor
+    ocifreestatement($mycursor); // close procedure call
+    ocifreestatement($outrefc); // close cursor
+
+    var_dump($data2);
+
+
+    echo "<div id='Evento_U' class='Evento_U'>
+  <div id='NombreEvento'class='Evento_Nombre'> $nombreEvento </div>
+  <div class='Fechas'>
+    <div class='Fecha'> $inicio </div>
+    <div class='del_al'> hasta </div>
+    <div class='Fecha'> $fin </div>
   </div>
-  <div id ="descripcion"class="Descripcion">ascascascascascascascascascascascascascsacccccccccccccccc</div>
-  <div id="GroupBox" class="GroupBox">
-    <div class="UnGrupo">
-      <div class="GroupName">Grupo A</div>
-      <div class="TeamRow">
-        <div class="ElTeamName">Cos ddvsvsta Rica</div>
-        <div class="stak partidos_Jugados">1</div>
-        <div class="stak partidos_Ganados">2</div>
-        <div class="stak partidos_Perdidos">3</div>
-        <div class="stak partidos_Empatados">4</div>
-        <div class="stak partidos_GolFavor">5</div>
-        <div class="stak partidos_GolContra">6</div>
-        <div class="stak partidos_GolDiferencia">7</div>
-        <div class="stak partidos_FairPlay">8</div>
-      </div>
+  <div id='GroupBox' class='GroupBox'>
+    <div class='UnGrupo'>
+      <div class='GroupName'>Grupo 0</div>";
+
+
+
+    
+
+
+
+
+$temporal=0;
+
+
+    for($k=0;$k<count($data2['FK_TEAM_ID']);$k++){
+      $equipo=$data2['FK_TEAM_ID'][$k];
+      $nuevoTemporal=$data2['FK_GROUP_ID'][$k];
+
+      $stid = oci_parse($conn, "begin :ret :=getNameTeamById($equipo); end;");
+      oci_bind_by_name($stid, ':ret', $nombreEquipo, 200);
+      oci_execute($stid);
+
+
+
+      $stid = oci_parse($conn, "begin :ret :=getTotalPartidos($equipo, $evento); end;");
+      oci_bind_by_name($stid, ':ret', $jugados, 200);
+      oci_execute($stid);
+
+      $stid = oci_parse($conn, "begin :ret :=getGanados($equipo, $evento); end;");
+      oci_bind_by_name($stid, ':ret', $ganados, 200);
+      oci_execute($stid);
+
+      $stid = oci_parse($conn, "begin :ret :=getPerdidos($equipo, $evento); end;");
+      oci_bind_by_name($stid, ':ret', $perdidos, 200);
+      oci_execute($stid);
+
+      $stid = oci_parse($conn, "begin :ret :=getPartidoenEvento($equipo, $evento); end;");
+      oci_bind_by_name($stid, ':ret', $aFavor, 200);
+      oci_execute($stid);
+
+      $stid = oci_parse($conn, "begin :ret :=getPartidoenEventoContra($equipo, $evento); end;");
+      oci_bind_by_name($stid, ':ret', $enContra, 200);
+      oci_execute($stid);
+
+      $stid = oci_parse($conn, "begin :ret :=getDiferencia($equipo, $evento); end;");
+      oci_bind_by_name($stid, ':ret', $diferencia, 200);
+      oci_execute($stid);
+
+
+echo "
+
+      <div class='TeamRow'>
+        <div class='ElTeamName'> $nombreEquipo </div>
+        <div class='stak partidos_Jugados'> $jugados </div>
+        <div class='stak partidos_Ganados'> $ganados </div>
+        <div class='stak partidos_Perdidos'> $perdidos </div>
+        <div class='stak partidos_GolFavor'> $aFavor </div>
+        <div class='stak partidos_GolContra'> $enContra </div>
+        <div class='stak partidos_GolDiferencia'> $diferencia </div>
+      </div>";
+
+    if($k!=0){
+        if($nuevoTemporal!=$temporal){
+          $temporal=$nuevoTemporal;
+      echo "
+
+          </div>
+        </div>
+
+    <div class='UnGrupo'>
+      <div class='GroupName'>Grupo $nuevoTemporal</div>
+
+";
+
+        
+      
+      }
+    }
+
+
+
+
+      }
+
+
+
+
+
+
+      ?>
+
+
+
+
     </div>
 
   </div>
